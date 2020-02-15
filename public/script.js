cce70b27ccdbda3ec77d72fc82d0ba79ab6f4a9e
Dallas W <dallasweinrich@outlook.com>
2020-02-15 09:13:09 -0800

    first commit

--- public/script.js
+++ public/script.js
@@ -1,1 +1,54 @@
-console.log("It works!");
+new Vue({
+    el: "#app",
+    data: {
+        total: 0,
+        products: [],
+        cart: [],
+        search: "",
+    },
+    methods: {
+        addToCart: function(product){
+            this.total += product.price;
+            var found = false;
+            for(var i = 0; i < this.cart.length; i++) {
+                if(this.cart[i].id === product.id) {
+                    this.cart[i].qty++
+                    found = true;
+                }
+            }
+            if(!found){
+                this.cart.push({
+                    id: product.id,
+                    title: product.title,
+                    price: product.price,
+                    qty: 1
+                });
+            }
+            
+        },
+        inc: function(item) {
+            item.qty++;
+            this.total += item.price;
+        },
+        dec: function(item){
+            item.qty--;
+            this.total -= item.price;
+            if(item.qty <= 0) {
+                var i = this.cart.indexOf(item);
+                this.cart.splice(i, 1);
+            }
+        },
+        onSubmit: function(){
+            var path = `/search?q=${this.search}`;
+            this.$http.get(path)
+                .then(function(response){
+                    this.products = response.body;
+                })
+        }
+    },
+    filters: {
+        currency: function(price){
+            return `$${price.toFixed(2)}`;
+        }
+    }
+});
