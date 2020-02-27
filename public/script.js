aa2ce98ea72f4fdc68161eb532201e8ef01a3de5
Dallas W <dallasweinrich@outlook.com>
2020-02-26 21:10:47 -0800

    Installed scrollMonitor

--- public/script.js
+++ public/script.js
@@ -1,56 +1,65 @@
+var LOAD_NUM = 4;
+
 new Vue({
-    el: "#app",
-    data: {
-        total: 0,
-        products: [],
-        cart: [],
-        search: "",
-        lastSearch: ""
-    },
-    methods: {
-        addToCart: function(product){
-            this.total += product.price;
-            var found = false;
-            for(var i = 0; i < this.cart.length; i++) {
-                if(this.cart[i].id === product.id) {
-                    this.cart[i].qty++
-                    found = true;
-                }
-            }
-            if(!found){
-                this.cart.push({
-                    id: product.id,
-                    title: product.title,
-                    price: product.price,
-                    qty: 1
-                });
-            }
-            
-        },
-        inc: function(item) {
-            item.qty++;
-            this.total += item.price;
-        },
-        dec: function(item){
-            item.qty--;
-            this.total -= item.price;
-            if(item.qty <= 0) {
-                var i = this.cart.indexOf(item);
-                this.cart.splice(i, 1);
-            }
-        },
-        onSubmit: function(){
-            var path = `/search?q=${this.search}`;
-            this.$http.get(path)
-                .then(function(response){
-                    this.products = response.body;
-                    this.lastSearch = this.search;
-                })
-        }
-    },
-    filters: {
-        currency: function(price){
-            return `$${price.toFixed(2)}`;
-        }
-    }
+	el: "#app",
+	data: {
+		total: 0,
+		products: [],
+		cart: [],
+		search: "cat",
+		lastSearch: "",
+        loading: false,
+        results: []
+	},
+	created: function() {
+		this.onSubmit();
+	},
+	methods: {
+		addToCart: function(product) {
+			this.total += product.price;
+			var found = false;
+			for (var i = 0; i < this.cart.length; i++) {
+				if (this.cart[i].id === product.id) {
+					this.cart[i].qty++;
+					found = true;
+				}
+			}
+			if (!found) {
+				this.cart.push({
+					id: product.id,
+					title: product.title,
+					price: product.price,
+					qty: 1
+				});
+			}
+		},
+		inc: function(item) {
+			item.qty++;
+			this.total += item.price;
+		},
+		dec: function(item) {
+			item.qty--;
+			this.total -= item.price;
+			if (item.qty <= 0) {
+				var i = this.cart.indexOf(item);
+				this.cart.splice(i, 1);
+			}
+		},
+		onSubmit: function() {
+			this.products = [];
+			this.loading = true;
+			var path = `/search?q=${this.search}`;
+			this.$http.get(path).then(function(response) {
+                this.results = response.body;
+				this.products = response.body.slice(0, LOAD_NUM);
+				this.lastSearch = this.search;
+				this.loading = false;
+			});
+		}
+	},
+	filters: {
+		currency: function(price) {
+			return `$${price.toFixed(2)}`;
+		}
+	}
 });
